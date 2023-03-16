d5d5cca685bf43bea6fd7301676837b9263e0527
MorsiBouhlel <31985111+MorsiBouhlel@users.noreply.github.com>
2023-03-16 10:23:15 +0100

    Update MenuItem.php

--- src/Entity/MenuItem.php
+++ src/Entity/MenuItem.php
@@ -45,12 +45,18 @@ class MenuItem implements MenuItemInterface
     protected ?string $style = null;
     
     /**
+     * @var Collection|ImageInterface
+     */
+    protected $images;
+    
+    /**
      * MenuItem constructor.
      */
     public function __construct()
     {
         $this->initializeTranslationsCollection();
         $this->items = new ArrayCollection();
+        $this->images = new ArrayCollection();
     }
 
     /**
@@ -198,5 +204,59 @@ class MenuItem implements MenuItemInterface
     protected function createTranslation(): MenuItemTranslationInterface
     {
         return new MenuItemTranslation();
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function getImages(): Collection
+    {
+        return $this->images;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getImagesByType(string $type): Collection
+    {
+        return $this->images->filter(function (ImageInterface $image) use ($type) {
+            return $type === $image->getType();
+        });
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function hasImages(): bool
+    {
+        return !$this->images->isEmpty();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function hasImage(ImageInterface $image): bool
+    {
+        return $this->images->contains($image);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addImage(ImageInterface $image): void
+    {
+        $image->setOwner($this);
+        $this->images->add($image);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function removeImage(ImageInterface $image): void
+    {
+        if ($this->hasImage($image)) {
+            $image->setOwner(null);
+            $this->images->removeElement($image);
+        }
     }
 }
